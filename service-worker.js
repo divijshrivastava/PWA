87d5a59ce3ff0386aa881b6b96179ca1dc25e03f
Divij <divij.shrivastava@gmail.com>
2024-09-21 19:45:54 +0530

    adds service worker

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,53 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('static-v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
-            ]);
-        })
-    );
-});
 
-self.addEventListener('fetch', (event) => {
+const cacheName = 'v1';
+
+self.addEventListener('install', e =>{
+    console.log("Installing SW");
+
+  /*  e.waitUntil(
+
+        
+
+    );*/
+})
+
+self.addEventListener('activate', e => {
+   // self.clients.claim();
+   e.waitUntil(clients.claim());
+   console.log("Activating.")
+})
+
+self.addEventListener('fetch', event => {
+
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+
+        fetch(event.request).then(resp =>{
+            const respClone = resp.clone();
+
+            caches.open(cacheName).then( cache =>{
+                console.log("Putting response in cache.")
+                cache.put(event.request, respClone).then(c =>{
+                    console.log("Successfully put in cache.")
+                })
+                .catch( err =>{
+                    console.log(`Cannot put in cache: ${err}`)
+                });
+            })
+             console.log("Returning response from the server.")
+
+            return resp;
+
+        }).catch( err => {
+            console.log(`Server down. ${err}`)
+            return caches.match(event.request)
+            .then(
+                resp => {
+                console.log("Return response from the cache.")
+                return resp;
+            })
         })
-    );
-});
+    )
+})
+
+
+
